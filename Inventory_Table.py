0bbeb0945a6203d5096ced9c5ef4b32fc4b4b01e
BishopRoss <bishopross@gmail.com>
2022-04-03 21:07:51 -0400

    Week 5 Project upload. Me, Straub, and Lawrence. We modified the access and log files to properly display entries from our access database. Bishop also added two new files: Employee_Obj.py and Inventory_Table.py to represent some of the data structures for the program. Bishop and Straub also edited the GUI file to reflect the proper field names.

--- Inventory_Table.py
+++ Inventory_Table.py
@@ -1,5 +1,5 @@
 # File for the class definition for the Inventory object
-# B Ross dd
+# B Ross ddd
 
 class Inventory:
     def __init__(self, itemName, inOut, assignTo, clearanceLevel, serialNumber):
