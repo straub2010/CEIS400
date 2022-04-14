b2f169e4d73714aa47c618fd5975d7dba64d8bdc
BishopRoss <bishopross@gmail.com>
2022-04-14 14:57:49 -0400

    Updated the query file. Previously, the database connector was declared in every function within this file, meaning if the file path had to be updated for the MS Access file then it had to be changed in each function. Now, the file path is declared at the top of the file and the functions use the variable the file path is set to so if the filepath needs to be updated, it only needs to be updated in one place.

--- Queries.py
+++ Queries.py
@@ -3,6 +3,9 @@ import datetime
 import pyodbc
 from datetime import datetime
 
+databaseConnector = pyodbc.connect(
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
+        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
 
 # B
 # Any where you see r'Driver = .....DBQ =, update the file path for the access
@@ -11,9 +14,7 @@ from datetime import datetime
 
 def retrieve_logs():
     results = []
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
-        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
+    conn = databaseConnector
     cursor = conn.cursor()
     cursor.execute('select * from employee')
     for row in cursor:
@@ -30,9 +31,7 @@ def showLogs():
 def updateTable(ser_num, status):
     ser_num = ser_num
     status = status
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
-        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
+    conn = databaseConnector
     cursor = conn.cursor()
     cursor.execute('UPDATE Inventory SET STATUS = 0 WHERE SERIAL_NUM=3')
     conn.commit()
@@ -40,9 +39,7 @@ def updateTable(ser_num, status):
 
 
 def insertRecord():
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
-        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
+    conn = databaseConnector
     cursor = conn.cursor()
     cursor.execute('''
     INSERT INTO Inventory (CLEAR_LEVEL, ITEM_NAME, STATUS)
@@ -56,9 +53,7 @@ def insertIntoLogsTableOut(emp_id, ser_num):
     dateAndTime = now.strftime('%m/%d/%Y %I:%M:%S %p')
     emp_id = emp_id
     ser_num = ser_num
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
-        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
+    conn = databaseConnector
     cursor = conn.cursor()
     print(dateAndTime)
     cursor.execute(
@@ -74,9 +69,7 @@ def insertIntoLogsTableIn(emp_id, ser_num):
     emp_id = emp_id
     ser_num = ser_num
     dateAndTime = now.strftime('%m/%d/%Y %I:%M:%S %p')
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
-        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
+    conn = databaseConnector
     cursor = conn.cursor()
     print(dateAndTime)
     cursor.execute(
@@ -89,9 +82,7 @@ def insertIntoLogsTableIn(emp_id, ser_num):
 
 def createRecord():
     results = []
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
-        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
+    conn = databaseConnector
     cursor = conn.cursor()
     cursor.execute('''SELECT Employee.F_NAME, Employee.L_NAME, Checkout_Logs.TIME_LOG, Checkout_Logs.LOG_TYPE, 
     Inventory.ITEM_NAME FROM (Checkout_Logs INNER JOIN Employee ON Checkout_Logs.EMP_ID = Employee.EMP_ID) INNER JOIN 
@@ -105,9 +96,7 @@ def createRecord():
 
 def loginFunction(username, password, employee_ID):
     users_info = []
-    conn = pyodbc.connect(
-        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\Bishop '
-        r'Ross\PycharmProjects\CEIS400\BishopsDatabase.accdb;')
+    conn = databaseConnector
     cursor = conn.cursor()
     cursor.execute(f"SELECT LOGIN_DETAILS.EMP_ID, LOGIN_DETAILS.USER, LOGIN_DETAILS.PASS FROM LOGIN_DETAILS WHERE "
                    f"EMP_ID = {employee_ID} and USER = '{username}' and PASS = '{password}'")
